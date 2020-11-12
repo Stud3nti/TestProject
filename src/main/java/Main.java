9a8714ed016b4549ccb845b0e868155db3d2975f
Student   WONG Isaac <08446@farnborough.ac.uk>
2020-11-12 13:48:18 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        String var = "Hello..dad..";
+        System.out.println(var);
+        System.out.println(var.length());
+
+    }
+}
