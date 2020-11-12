8340901aaa15fa4188ff52cd1dc477243401db8c
iwong04 <08446@farnborough.ac.uk>
2020-11-12 13:51:34 +0000

    yooooooo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
-        String var = "Hello..dad..";
+        String var = "Hello..You..";
         System.out.println(var);
         System.out.println(var.length());
 
